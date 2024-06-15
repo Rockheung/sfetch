94a9662224b190c7ef557d595cd5441e22091640
heungjun.park <rockheung@gmail.com>
2024-06-15 19:48:57 +0900

    feat: success getting set-cookie from edge fetching

--- src/index.ts
+++ src/index.ts
@@ -11,21 +11,128 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import axios, { Axios, AxiosHeaders, AxiosPromise, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+
+type Serializable = string | number | boolean | null | undefined | Serializable[] | { [key: string]: Serializable };
+
+const convertAxiosHeaderToHeaders = (headers: AxiosHeaders): Headers => {
+	const headersObj = new Headers();
+	if (typeof headers.toJSON === 'function') {
+		const headersObj = headers.toJSON();
+		for (const key in headersObj) {
+			if (Object.prototype.hasOwnProperty.call(headersObj, key)) {
+				const value = headersObj[key];
+				switch (typeof value) {
+					case 'string':
+						headers.set(key, value);
+						break;
+					case 'object':
+						if (Array.isArray(value)) {
+							value.forEach(v => headers.append(key, v));
+						}
+						break;
+				}
+			}
+		}
+	}
+	return headersObj;
+}
+
+const convertHeadersToAxiosHeaders = (headers: Headers): AxiosHeaders => {
+	const headersObj = new AxiosHeaders();
+
+	for (const [key, value] of headers.entries()) {
+		if (key === 'set-cookie') {
+			headersObj.set('set-cookie', headers.getSetCookie().join(', '));
+			continue;
+		}
+		headersObj.set(key, value);
+	}
+	return headersObj;
+}
+
+const fetchAdapter = async (config: InternalAxiosRequestConfig): Promise<AxiosResponse> => {
+	const request = new Request(config.url!, {
+		method: config.method,
+		headers: convertAxiosHeaderToHeaders(config.headers),
+		body: config.data,
+		redirect: 'manual',
+	});
+	const res = await fetch(request);
+
+	return {
+		data: await res.text(),
+		status: res.status,
+		statusText: res.statusText,
+		headers: convertHeadersToAxiosHeaders(res.headers),
+		config,
+	}
+}
+
+const iAxios = axios.create({
+	adapter: fetchAdapter,
+	maxRedirects: 0,
+	withCredentials: true,
+	validateStatus: _ => true,
+	transformRequest: _ => _,
+	transformResponse: _ => _,
+});
+
+
+
+
 // import handleProxy from './proxy';
 // import handleRedirect from './redirect';
 // import apiRouter from './router';
-import http from 'node:http';
-import https from 'node:https';
 
-
+// nTitleNo=126410663&nApiLevel=10&nPlaylistIdx=0
 
 // Export a default object containing event handlers
 export default {
 	// The fetch handler is invoked when this worker receives a HTTP(S) request
 	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello, world!', {
-			status: 200,
-		});
+		try {
+			const requestConfig = await request.json<AxiosRequestConfig>();
+			const res = await iAxios.request(requestConfig);
+			console.log(typeof res.headers['set-cookie'])
+			const headers = new Headers();
+			if (typeof res.headers.toJSON === 'function') {
+				const headersObj = res.headers.toJSON();
+				for (const key in headersObj) {
+					if (Object.prototype.hasOwnProperty.call(headersObj, key)) {
+						const value = headersObj[key];
+						switch (typeof value) {
+							case 'string':
+								headers.set(key, value);
+								break;
+							case 'object':
+								if (Array.isArray(value)) {
+									value.forEach(v => headers.append(key, v));
+								}
+								break;
+						}
+					}
+				}
+			} else {
+
+			}
+			headers.set('X-From-Saxios', res.headers['content-type']);
+      return new Response(res.data, {
+				statusText: res.statusText,
+				status: res.status,
+				headers,
+			});
+    } catch (error) {
+			if (error instanceof Error) {
+				return new Response(error.stack,{
+					status: 400
+				});
+			}
+
+			return new Response('An unknown error occurred',{
+				status: 500
+			});
+    }
 	}
 };
